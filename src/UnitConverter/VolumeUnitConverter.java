3b225c796e13ec49ac6e370ed30b1c0e9288f0e7
KenanFa3 <Kenan.fayoumi@ozu.edu.tr>
2018-12-15 21:44:43 +0300

    Fully functional unit converter

--- src/UnitConverter/VolumeUnitConverter.java
+++ src/UnitConverter/VolumeUnitConverter.java
@@ -10,7 +10,28 @@ public class VolumeUnitConverter implements UnitConverter {
     @Override
     public double convert(String fromUnit, String toUnit, double value) {
 
-    return 0.01;
+        double valueInLitre = convertToLitre(fromUnit, value);
+
+        double multiplier = 1;
+        switch (toUnit.toLowerCase()) {
+            case "gallon":
+                multiplier = LITRE_TO_GALLON;
+                break;
+        }
+        return multiplier * valueInLitre;
+
 
     }
+
+    private double convertToLitre(String fromUnit, double value) {
+
+        double multiplier = 1;
+        switch (fromUnit.toLowerCase()) {
+            case "gallon":
+                multiplier = GALLON_TO_LITRE;
+                break;
+        }
+        return multiplier * value;
+    }
+
 }
--- src/UnitConverter/WeightUnitConverter.java
+++ src/UnitConverter/WeightUnitConverter.java
@@ -2,8 +2,35 @@ package UnitConverter;
 import  Factories.UnitConverter;
 public class WeightUnitConverter implements UnitConverter {
 
+    private static final double KILOGRAMS_TO_POUND = 2.20462;
+    private static final double POUND_TO_KILOGRAMS = 0.453592;
+
     @Override
     public double convert(String fromUnit, String toUnit, double value) {
-        return 123213125;
+
+        double valueInKilograms = convertToKilograms(fromUnit, value);
+
+        double multiplier = 1;
+        switch (toUnit.toLowerCase()) {
+            case "pound":
+                multiplier = KILOGRAMS_TO_POUND;
+                break;
+        }
+        return multiplier * valueInKilograms;
+
+
     }
+
+    private double convertToKilograms(String fromUnit, double value) {
+
+        double multiplier = 1;
+        switch (fromUnit.toLowerCase()) {
+            case "pound":
+                multiplier = POUND_TO_KILOGRAMS;
+                break;
+        }
+        return multiplier * value;
+    }
+
+
 }
